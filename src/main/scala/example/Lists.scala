6ea8f4a35ecdfd21be937099dae58dc5d048e2ad
Rebeca Perez <caberezerep@gmail.com>
2019-09-24 21:54:59 +0200

    Initial step exercise 1

--- src/main/scala/example/Lists.scala
+++ src/main/scala/example/Lists.scala
@@ -23,7 +23,13 @@ object Lists {
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-    def sum(xs: List[Int]): Int = ???
+    def sum(xs: List[Int]): Int = {
+       if (xs.isEmpty){
+            0
+       } else {
+            xs.head +  sum(xs.tail)
+       }
+    }
   
   /**
    * This method returns the largest element in a list of integers. If the
@@ -38,5 +44,20 @@ object Lists {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-    def max(xs: List[Int]): Int = ???
+    def max(xs: List[Int]): Int = {
+        max_aux(xs.head, xs.tail)
+
+    }
+
+    def max_aux( xs: Int, xy: List[Int]): Int = {
+      if (xy.isEmpty){
+                xs
+      }else{
+               if (xs >= xy.head) {
+                      max_aux(xs, xy.tail)
+               }else{
+                      max_aux(xy.head, xy.tail)
+               }
+      }
+    }
   }
